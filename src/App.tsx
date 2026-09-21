5dc5fb23389b88c29ebedce18bcd41bd762a190f
pv palo <luisferdepaz@prodigiosovolcan.com>
2026-09-21 12:05:09 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Frame, StoryboardConfig } from './types';
 import { Sidebar } from './components/Sidebar';
 import { ScriptParser } from './components/ScriptParser';
